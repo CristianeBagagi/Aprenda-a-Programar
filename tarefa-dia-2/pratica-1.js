feef61bbcd22dc1c5994a0509655f869b5fa83aa
Cristiane Bagagi <crisbagagi@gmail.com>
2022-08-26 10:39:11 -0300

    dadospessoais

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,3 +9,10 @@ email = crisbagagi@gmail.com
 profissao = bancaria
 idade = 39
 sonho = Paris
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(sonho)
+
