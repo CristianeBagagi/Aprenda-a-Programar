9443acaf303327ea9d00d067343cd121f36e43f2
Cristiane Bagagi <crisbagagi@gmail.com>
2022-08-30 09:55:36 -0300

    pratica1e2

--- tarefa-dia-3/pratica-dia-1.js
+++ tarefa-dia-3/pratica-dia-1.js
@@ -1,14 +1,9 @@
 
 
-let multiplicador = 10
-let n = 10
-let i = 1
+let contador  = 1
 
-while (i <=n) {  
-    let resultado = multiplicador * i
-    let tabuada = `${multiplicador} X ${i} = ${resultado}`
-    console.log (tabuada)
-    i++
+
+while (contador <= 10) {  
+    console.log(contador)
+    contador = contador + 1
 }
-
-
